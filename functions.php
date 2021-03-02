b64b338166dc68c149a67a12db595263a17dee3f
Steve Ryan <steve.ryan@asu.edu>
2021-03-02 13:08:28 -0700

    Update functions.php to reference included JS file.
    Gulp script does not yet produce a minified version of the JS file. Temporary patch to fix a PHP error upon installation.

--- functions.php
+++ functions.php
@@ -12,6 +12,6 @@ function uds_wordpress_child_scripts() {
     $css_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/css/child-theme.min.css' );
     wp_enqueue_style( 'uds-wordpress-child-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array('uds-wordpress-styles'), $css_child_version );
 
-    $js_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/js/child-theme.min.js' );
-    wp_enqueue_style( 'uds-wordpress-child-styles', get_stylesheet_directory_uri() . '/js/child-theme.min.js', array('jquery'), $js_child_version );
+    $js_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/js/child-theme.js' );
+    wp_enqueue_style( 'uds-wordpress-child-styles', get_stylesheet_directory_uri() . '/js/child-theme.js', array('jquery'), $js_child_version );
 }
