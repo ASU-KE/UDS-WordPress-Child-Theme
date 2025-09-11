451868aac9fea1b2dd4afef9bb62bcb09afcf544
John Cox <john.k.cox@asu.edu>
2025-09-11 11:46:11 -0700

    enqueue css update

--- functions.php
+++ functions.php
@@ -18,8 +18,8 @@ function uds_wordpress_child_scripts() {
 	$the_theme     = wp_get_theme();
 	$theme_version = $the_theme->get( 'Version' );
 
-	$css_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/css/child-theme.min.css' );
-	wp_enqueue_style( 'uds-wordpress-child-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array( 'uds-wordpress-styles' ), $css_child_version );
+	$css_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/dist/css/child-theme.min.css' );
+	wp_enqueue_style( 'uds-wordpress-child-styles', get_stylesheet_directory_uri() . '/dist/css/child-theme.min.css', array( 'uds-wordpress-styles' ), $css_child_version );
 
 	$js_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/dist/js/theme.min.js' );
 	wp_enqueue_script( 'uds-wordpress-child-scripts', get_stylesheet_directory_uri() . '/dist/js/theme.min.js', array( 'jquery' ), $js_child_version );
