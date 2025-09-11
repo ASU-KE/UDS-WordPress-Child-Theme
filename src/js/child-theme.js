f91586b35b5efe79fdac4cac3869e822319459f3
John Cox <john.k.cox@asu.edu>
2025-09-11 11:17:49 -0700

    blank js init

--- src/js/child-theme.js
+++ src/js/child-theme.js
@@ -1,6 +1,1 @@
-// Start your project's JS here.
-// If you are rolling some jQuery you'll want to use a doc ready statement like the following.
-
-jQuery(document).ready(function($){
-  // Code is poetry.
-});
+/* Child theme JavaScript */
