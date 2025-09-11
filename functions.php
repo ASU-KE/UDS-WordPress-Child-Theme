829059797defa69196654adfa568adf9afb0ad0a
John Cox <john.k.cox@asu.edu>
2025-09-11 11:19:26 -0700

    update enqueue to dist js

--- functions.php
+++ functions.php
@@ -21,8 +21,8 @@ function uds_wordpress_child_scripts() {
 	$css_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/css/child-theme.min.css' );
 	wp_enqueue_style( 'uds-wordpress-child-styles', get_stylesheet_directory_uri() . '/css/child-theme.min.css', array( 'uds-wordpress-styles' ), $css_child_version );
 
-	$js_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/js/child-theme.js' );
-	wp_enqueue_script( 'uds-wordpress-child-styles', get_stylesheet_directory_uri() . '/js/child-theme.js', array( 'jquery' ), $js_child_version );
+	$js_child_version = $theme_version . '.' . filemtime( get_stylesheet_directory() . '/dist/js/theme.min.js' );
+	wp_enqueue_script( 'uds-wordpress-child-scripts', get_stylesheet_directory_uri() . '/dist/js/theme.min.js', array( 'jquery' ), $js_child_version );
 }
 add_action( 'wp_enqueue_scripts', 'uds_wordpress_child_scripts' );
 
